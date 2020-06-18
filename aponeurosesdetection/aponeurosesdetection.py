17757229adc186dcddff361b321762bd5c95bf77
Lisa-pa <65156893+Lisa-pa@users.noreply.github.com>
2020-06-18 14:35:11 +0200

    Move a part from main program to  tests directory

--- aponeurosesdetection/aponeurosesdetection.py
+++ aponeurosesdetection/aponeurosesdetection.py
@@ -113,30 +113,6 @@ def MVEF_2D(I, scales, thresholds):
     return I2, vesselness, hessian_xy, hessian_xx, hessian_yy
 '-----------------------------------------------------------------------------'
 
-"*****************************************************************************"
-"*********************************PROGRAM*************************************"
-"*****************************************************************************"
-
-'Opening of the image and color transform.'
-RGB_image = cv2.imread('cropped0.jpg', -1);
-gray_I = cv2.cvtColor(RGB_image, cv2.COLOR_RGB2GRAY);
-gray_I2 = 255 - gray_I; #inverse black and white
-#gray_I2 = cv2.equalizeHist(gray_I);
-
-'Multiscale Vessel Enhancement Method'
-seg, vesselness,hessXY, hessXX, hessYY = MVEF_2D(gray_I2, [4.], [0.5, 0.5]);
-
-'Rescale the pixels values if max. value is too low'
-maxi = np.max(seg);
-if maxi <=0.5 and maxi>0:
-    seg = (seg/maxi)*255;
-
-'Visualization'
-cv2.imshow('MVEF',seg);
-cv2.imshow('Initial image', gray_I);
-cv2.imshow('Inverse image', gray_I2);
-cv2.waitKey(0) & 0xFF;
-cv2.destroyAllWindows();
 
 "*****************************************************************************"
 "*****************************************************************************"
--- tests/test_aponeurosesdetection.py
+++ tests/test_aponeurosesdetection.py
@@ -21,6 +21,25 @@ class TestAponeurosesdetection(unittest.TestCase):
 
     def test_000_something(self):
         """Test something."""
+        'Opening of the image and color transform.'
+        RGB_image = cv2.imread('cropped0.jpg', -1);
+        gray_I = cv2.cvtColor(RGB_image, cv2.COLOR_RGB2GRAY);
+        gray_I2 = 255 - gray_I; #inverse black and white
+
+        'Multiscale Vessel Enhancement Method'
+        seg, vesselness,hessXY, hessXX, hessYY = MVEF_2D(gray_I2, [4.], [0.5, 0.5]);
+
+        'Rescale the pixels values if max. value is too low'
+        maxi = np.max(seg);
+        if maxi <=0.5 and maxi>0:
+            seg = (seg/maxi)*255;
+
+        'Visualization'
+        cv2.imshow('MVEF',seg);
+        cv2.imshow('Initial image', gray_I);
+        cv2.imshow('Inverse image', gray_I2);
+        cv2.waitKey(0) & 0xFF;
+        cv2.destroyAllWindows();
 
     def test_command_line_interface(self):
         """Test the CLI."""
