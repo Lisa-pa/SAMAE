109a3c6b430a2f9192283939bb8e145540db8b0a
AJMA <ajmoralesartacho@gmail.com>
2020-07-03 23:43:13 +0200

    add script file

---
+++ script_draft.py
@@ -0,0 +1,18 @@
+from skimage.color import rgb2gray
+import cv2
+
+from aponeurosesdetection.aponeuroses_contours import apoContour
+import aponeurosesdetection.data as dt
+
+# Import sample images 
+cirimg = rgb2gray(dt.circle()) # circle sample img
+skimg = rgb2gray(dt.skmuscimg()) # ultrasound sample img
+
+# Let's test apoContour
+pt1 = [76,85]
+contour, n, maxiDPHI, LIF, GIF = apoContour(cirimg, pt1, 1.0, 1.0, 1.0, 65.025, 0.15, 1, 0.01, 3.0, 0.1)
+
+cv2.imshow('Initial I',cirimg)
+cv2.imshow('LGIF',contour)
+cv2.waitKey(0) & 0xFF
+cv2.destroyAllWindows()
