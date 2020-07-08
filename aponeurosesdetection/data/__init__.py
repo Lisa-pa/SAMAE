fd06cb2d00307cc77b228a9184f1322af629e470
Lisa-pa <65156893+Lisa-pa@users.noreply.github.com>
2020-07-08 15:23:16 +0200

    Add function to download a file from Dropbox

--- aponeurosesdetection/data/__init__.py
+++ aponeurosesdetection/data/__init__.py
@@ -46,3 +46,40 @@ def skmuscimg():
     """
 
     return _load(os.path.join(data_dir, "skmuscle.jpg"))
+
+
+def downloadFromDropbox(tok, path2file):
+    """Downloads a file from a dropbox account
+
+    Args:
+        tok (string): access token that connects to the wanted
+                      app in Dropbox account
+        path2file (string): Path of the file to download, in the
+                            app corresponding to the upward token.
+
+    Output:
+            image (numpy.ndarray): 3-channel color image, with 
+                                    coefficients' type == uint8
+
+    Example (not working yet, I have to find a way to generate tokens that
+    to not give total access to my dropbox account but only to the desired
+    file):
+        > token = 'randomstring_887_85sf84654f5dfdfggkf'
+        > path = '/cropped_20181002_153426_image.jpg'
+        > dt = downloadFromDropbox(token, path);
+    """
+    import dropbox
+    import numpy as np
+    import cv2
+
+    dbx  = dropbox.Dropbox(tok)
+    try:
+        metadata, file = dbx.files_download(path2file)
+    except dropbox.exceptions.HttpError as err:
+        print('*** HTTP error', err)
+        return None
+       
+    data = np.frombuffer(file.content, np.uint8)
+    image = cv2.imdecode(data, 1) # cv2.IMREAD_COLOR in OpenCV 3.1
+
+    return image
