78460a4985913235026d56d3d8ffdeb98a8e104b
Lisa-pa <65156893+Lisa-pa@users.noreply.github.com>
2020-07-20 14:45:56 +0200

    Re-write active contour model

--- aponeurosesdetection/aponeuroses_contours.py
+++ aponeurosesdetection/aponeuroses_contours.py
@@ -2,75 +2,49 @@ import cv2
 import math as m
 import numpy as np
 
-
-def heaviside(matrix, epsilon):
-    """Calculates the smooth heaviside function for each
-    coefficient of matrix.
-        h : R -> [-pi/2 ; pi/2]
-            x -> 1/2 * (1 + 2/pi * arctan( x /epsilon ))
+def gaussianKernel(sigma):
+    """ Creates a 2D Gaussian-like Kernel with standard deviation sigma,
+    centered on the middle of the kernel.
+    Its size is taken as 4*sigma+1.
 
     Args:
-        matrix (numpy array): 2D array_like object which can be
-        of any shape and contains real coefficients (doubles).
-        epsilon (double): real and strictly positive constant. 
+        sigma (double): standard deviation
 
     Returns:
-        numpy array: 2D array. Each coefficient is the heaviside value of
-        the matrix's coefficient at same location.
+        numpy array: 2D array of size (4*sigma+1)x(4*sigma+1).
     """
-    if epsilon <=0:
-        err = 'Error : epsilon must be strictly positive'
-        print(err)
-    else:
-        h = np.zeros((matrix.shape))
-        for u in range(matrix.shape[0]):
-            h[u, :] = np.array([1/2.*(1+2.*m.atan(matrix[u,v]/epsilon)/m.pi) for v in range(matrix.shape[1])])
-        return h
+    sizeK = 4 * int(sigma) + 1
+    a = np.ogrid[-int(sizeK/2):int(sizeK/2)+1, -int(sizeK/2):int(sizeK/2)+1][0]*np.ones((sizeK, sizeK))
+    b = np.ogrid[-int(sizeK/2):int(sizeK/2)+1, -int(sizeK/2):int(sizeK/2)+1][1]*np.ones((sizeK, sizeK))
+    kernel = 1/(2*math.pi*sigma*sigma) * np.exp(-(a**2 + b**2)/(2*sigma*sigma))
+    return kernel
 
-
-def gaussianKernel(ksize, point, s):
-    """ Creates a 2D Gaussian-like Kernel of ksize with standard deviation s.
-    The kernel is centered on point, and its coefficients decrease with distance 
-    from point = (abs, ord):
-            g : ZxZ -> R
-                (x, y) -> 1/(2*pi*s*s) * exp( - ( (x-abs)**2 + (y-ord)**2 ) / (2*s*s) )
-
-    Args:
-        ksize (tuple or array_like): the two first coefficients of ksize give the size of the gaussian kernel.
-        point (tuple or array_like): center point from which all distances are calculated
-        s (double): standard deviation
-
-    Returns:
-        numpy array: 2D array. Each coefficient has a value reflecting its distance from
-        point. The more distant from point, the smaller value.
-    """
-
-    absc = point[0]
-    ordo = point[1]
-    k = np.zeros(size)
-    for a in range(size[0]):
-        k[a, :] = np.array([1/(2*m.pi*s**2) * np.exp(-((a-absc)**2 + (b-ordo)**2)/(2*s*s))for b in range(size[1])])
-    return k
-
-
-def intensities(I, C, s, epsilon):
+def intensities(I, previousPhi, eps, s, l1, l2):
     """Calculates global and local intensities in two regions
-    of image I. These two regions are delineated by the contour C.
+    of image I. These two regions are delineated by a contour
+    modeled as the zero level set of a Lipschitz function Phi.
+    
         - c1 is the global intensity outside the contour
         - c2 is the global intensity inside the contour
         - f1 is a matrix of local intensities outside the contour
         - f2 is a matrix of local intensities inside the contour
+        - LIF is a matrix where each coefficient is the local 
+        intensity force of the corresponding pixel in I
+        - GIF is a matrix where each coefficient is the global 
+        intensity force of the corresponding pixel in I
 
     Args:
         I (numpy array): 2D image
-        C (numpy array): 2D matrix of a level set function that
+        previousPhi (numpy array): 2D matrix of a level set function that
         modelizes a closed contour - See references
+        eps (double): parameter used in dirac and heaviside functions approximation
         s (int): standard deviation of gaussian kernels
         epsilon (double): strictly positive constant
+        l1, l2 (double): strictly positive constants 
 
     Returns:
-        tuple: (c1,c2,f1,f2) where c1, c2 are doubles and
-        f1, f2 are 2D numpy arrays
+        tuple: (c1,c2,f1,f2, LIF, GIF) where c1, c2 are doubles and
+        f1, f2, LIF, GIF are 2D numpy arrays
 
     References:
         Based on [ Li Wang, Chunming Li, Quansen Sun, Deshen Xia, Chiu-Yen Kao,
@@ -79,32 +53,88 @@ def intensities(I, C, s, epsilon):
         and Graphics 33 (2009) 520–531 ]
     """
 
-    heavisideMatrix = heaviside(C, epsilon)        
-    OneMinusHeaviside = np.ones((I.shape[0],I.shape[1])) - heavisideMatrix
-    HI = heavisideMatrix*I
-    OneMinusH_I = OneMinusHeaviside*I
-
-    #local intensities
-    convolHI = cv2.GaussianBlur(HI, ksize = (2*int(s)+1,2*int(s)+1), sigmaX = s, sigmaY =s, borderType=cv2.BORDER_CONSTANT)
-    convolH = cv2.GaussianBlur(heavisideMatrix, ksize = (2*int(s)+1,2*int(s)+1),sigmaX = s,sigmaY = s, borderType=cv2.BORDER_CONSTANT)
-    convol1minusH_I = cv2.GaussianBlur(OneMinusH_I, ksize = (2*int(s)+1,2*int(s)+1),sigmaX =s, sigmaY =s, borderType=cv2.BORDER_CONSTANT)
-    convol1minusH = cv2.GaussianBlur(OneMinusHeaviside, ksize = (2*int(s)+1,2*int(s)+1), sigmaX =s, sigmaY =s, borderType=cv2.BORDER_CONSTANT)
+    Id = np.ones(I.shape)
+    K = gaussianKernel(s)
+    H = (1 + np.arctan(previousPhi/eps)*2/math.pi)/2
+    HI = H*I
+    oneMH = 1-H
+    oneMHI = oneMH*I
     
-    f1 = convolHI/convolH
-    f2 = convol1minusH_I/convol1minusH
+    c1 = np.sum(HI)/np.sum(H)
+    c2 = np.sum(oneMHI)/np.sum(oneMH)
+    
+    KHI = cv2.GaussianBlur(HI, ksize = (2*int(s)+1,2*int(s)+1), sigmaX = s, sigmaY =s, borderType=cv2.BORDER_DEFAULT)
+    KH = cv2.GaussianBlur(H, ksize = (2*int(s)+1,2*int(s)+1),sigmaX = s,sigmaY = s, borderType=cv2.BORDER_DEFAULT)
+    K1HI = cv2.GaussianBlur(oneMHI, ksize = (2*int(s)+1,2*int(s)+1),sigmaX =s, sigmaY =s, borderType=cv2.BORDER_DEFAULT)
+    K1H = cv2.GaussianBlur(oneMH, ksize = (2*int(s)+1,2*int(s)+1), sigmaX =s, sigmaY =s, borderType=cv2.BORDER_DEFAULT)
+    
+    f1 = KHI/KH
+    f2 = K1HI/K1H
+    
+    GIF = -l1 * (I- c1)*(I-c1) + l2 * (I-c2)*(I-c2)
+    
+    LIF = (l2 - l1) * I*I * signal.fftconvolve(Id, K, mode='same')\
+    -2 * I * signal.fftconvolve(-l1 * f1 + l2 * f2, K, mode='same')\
+    + signal.fftconvolve(-l1 * f1*f1 + l2 * f2*f2, K, mode='same')
+    
+    return c1, c2, f1, f2, LIF, GIF
 
-    #global intensity
-    c1 = np.sum(HI)/np.sum(heavisideMatrix)
-    c2 = np.sum(OneMinusH_I)/np.sum(heavisideMatrix)
-           
-    return c1, c2, f1, f2
+def initiateContour(I, typeC='circle', pointIni = None, setPoints = None):
+   """Create an initial contour for I as a zero level set function. The
+   shape of this contour depends on typeC.
 
+   Args:
+        I (array_like): one canal image
+        typeC (string): type of contour to draw. It can be 'circle',
+        or 'set_of_points' (still in development).
+        pointIni (tuple or list, optional): center of the circle if
+        'circle' is chosen
+        setPoints (array or list, optional): list of points to interpolate
+        so that to draw a closed curve from the points (if 'set_of_points' is chosen)
 
-def apoContour(I, pointIni, l1, l2, mu, nu, dt, epsilon, omega, sigma, stop_thresh):
+    Returns:
+        array_like ofsame size than I. The initial contour is caracterized by zero values
+        in the array.
+
+   """
+    initialPhi = np.zeros(I.shape)
+    
+    if typeC=='circle':
+        if pointIni is None:
+            raise TypeError('Missing center point to draw circle')
+        else:
+            for i in range(I.shape[0]):
+                for j in range(I.shape[1]):
+                    initialPhi[i,j] = -3 + np.sqrt((pointIni[0]-i)**2\
+                                       + (pointIni[1]-j)**2)
+   
+    if typeC == 'set_of_points':
+       print('not ready')
+
+        # if setPoints is None:
+        #     raise TypeError('Missing list setPoints')
+        # else:
+            
+        #     pad = 3
+        #     setPoints = np.pad(setPoints, [(pad,pad), (0,0)], mode='wrap')
+        #     x, y = setPoints.T
+        #     i = np.arange(0, len(setPoints))
+            
+        #     interp_i = np.linspace(pad, i.max() - pad + 1, 5 * (i.size - 2*pad))
+        #     xi = interp1d(i, x, kind='cubic')(interp_i) #ou interp2D
+        #     yi = interp1d(i, y, kind='cubic')(interp_i)
+        #     contour = np.array([])#np array 2D where a line is a point from the contour
+        #     for i in range(I.shape[0]):
+        #         for j in range(I.shape[1]):
+        #             initialPhi[i,j] = cv2.pointPolygonTest(contour, [i,j], True)
+
+    return initialPhi
+
+def activeContour(I, contourIni, thresh, l1, l2, s, eps, mu, nu, dt):
     """Determines the contour of an object in image I by recurrence. This function
-    acts like a snake function. An initial contour is generated around the point
-    pointIni. The contour then evoluates until a stationary solution is found, that is:
-    |new contour - previous contour| < stop_thresh.
+    acts like a snake function. The contour evoluates from the input contourIni until 
+    a stationary solution is found, that is:
+    |new contour - previous contour| < thresh.
     The chosen norm is the maximum among |new contour (x,y) - previous contour (x,y)| for
     (x,y) in the image domain.
     The evolution of the contour is provided by :
@@ -113,19 +143,19 @@ def apoContour(I, pointIni, l1, l2, mu, nu, dt, epsilon, omega, sigma, stop_thre
 
     Args:
         I (array_like): one-canal image
-        pointIni (array_like or tuple): contains two coefficients = [x0, y0]
+        contourIni (array_like of same size than I): contains the zero level
+        set initial values
         l1 (double): stricly positive constant
         l2 (double): stricly positive constant
+        s (double): standard deviation for gaussian kernels 
         mu (double): stricly positive constant that weights the level set regularization term
         nu (double): stricly positive constant that weights the length term
         dt (double): time step
-        epsilon (double): strictly positive constant
-        omega (double): constant between 0 and 1 that weights the influence of global and local intensities
-        sigma (double): standard deviation for gaussian kernels 
-        stop_thresh (double): stopping condition for the recurrence
+        eps (double): strictly positive constant
+        thresh (double): stopping condition for the recurrence
 
     Returns:
-        array_like: stationary contour
+        array_like: stationary contour. The array has the same size than I.
     
     References:
         Li Wang et al., Computerized Medical Imaging and Graphics 33 (2009) 520–531
@@ -137,89 +167,62 @@ def apoContour(I, pointIni, l1, l2, mu, nu, dt, epsilon, omega, sigma, stop_thre
     """
     
     if len(I.shape) > 2:
-        I = cv2.cvtColor(I, cv2.COLOR_RGB2GRAY)
+        I = cv2.cvtColor(I, cv2.COLOR_RGB2GRAY);
         
-    'Initialization step'
-    previousC = np.zeros((I.shape[0], I.shape[1])) #circle, center = point, diameter = 4
-    for i in range(I.shape[0]):
-        for j in range(I.shape[1]):
-            previousC[i,j] = -2 + m.sqrt((i-pointIni[0])**2 + (j - pointIni[1])**2)
-    test = 1000.
-    step = 0
+    'Initialization'
+    previousPhi = contourIni
     
-    'Recurrence'
-    #I temporarily added a condition in while loop to limit the number of iterations
-    while test> stop_thresh and step<=16:
-        dirac = np.zeros((I.shape[0], I.shape[1]))
+    'recurrence'
+    step = 1
+    stop_criterion = thresh + 1.
+    while stop_criterion > thresh and step <= 100000:
         
-        #Vectorization for laplacian and divergence calculations
-        #in the following names, P means Plus and M means Minus
-        prevC_iP1 = np.concatenate((previousC[1:,:],np.zeros((1,previousC.shape[1]))), axis = 0)
-        prevC_iM1 = np.concatenate((np.zeros((1,previousC.shape[1])),previousC[:previousC.shape[0]-1,:]), axis = 0)
-        prevC_jP1 = np.concatenate((previousC[:,1:],np.zeros((previousC.shape[0],1))), axis = 1)
-        prevC_jM1 = np.concatenate((np.zeros((previousC.shape[0],1)),previousC[:,:previousC.shape[1]-1]), axis = 1)
-        prevC_ijM1 = np.concatenate((np.zeros((previousC.shape[0],1)), np.concatenate((np.zeros((1,previousC.shape[1]-1)), previousC[:-1,:-1]),axis = 0)),axis = 1)
-        prevC_ijP1 = np.concatenate((np.concatenate((previousC[1:,1:],np.zeros((1,previousC.shape[1]-1))),axis = 0), np.zeros((previousC.shape[0],1))),axis = 1)
-        prevC_iP1jM1 = np.concatenate((np.zeros((previousC.shape[0],1)), np.concatenate((previousC[1:,:-1], np.zeros((1,previousC.shape[1]-1))),axis = 0)),axis = 1)
-        prevC_iM1jP1 = np.concatenate((np.concatenate((np.zeros((1,previousC.shape[1]-1)),previousC[:-1,1:]),axis = 0), np.zeros((previousC.shape[0],1))),axis = 1)
+        c1, c2, f1, f2, GIF, LIF = intensities(I, previousPhi, eps, s, l1, l2)
 
-        norm = np.ones(previousC.shape)/np.sqrt(\
-                      (prevC_iP1-previousC)*(prevC_iP1-previousC)\
-                      +(prevC_jP1-previousC)*(prevC_jP1-previousC))
+        w = 0.01
+        dirac = eps/(math.pi*(eps*eps+previousPhi*previousPhi))        
+        grad = np.gradient(previousPhi)
+        normgrad = np.sqrt(grad[0]*grad[0] + grad[1]*grad[1]);
+        normgrad = normgrad + (normgrad == 0)*1.
+        div = np.gradient(grad[0]/normgrad, axis = 0) + np.gradient(grad[1]/normgrad, axis = 1);
+        lap = cv2.Laplacian(previousPhi, cv2.CV_64F);
         
-        divergence = (np.ones(previousC.shape)+1/4*(prevC_iP1-prevC_iM1)*(prevC_iP1-prevC_iM1))\
-        *(prevC_iP1+prevC_iM1-2*previousC)\
-        +(np.ones(previousC.shape)+1/4*(prevC_jP1-prevC_jM1)*(prevC_jP1-prevC_jM1))\
-        *(prevC_jP1+prevC_jM1-2*previousC)\
-        +1/8*(prevC_iP1-prevC_iM1)*(prevC_jP1-prevC_jM1)\
-        *(prevC_ijP1-prevC_iP1jM1-prevC_iM1jP1-prevC_ijM1)
+        dPhi = dirac * ((1-w)*LIF + w*GIF)\
+                + nu * dirac * div\
+                + mu * (lap - div)
         
-        div = divergence / norm
+        newPhi = previousPhi + dt * dPhi
         
-        lap = prevC_iP1 + prevC_iM1 + prevC_jP1 + prevC_jM1\
-        -4*previousC
-                
-        #GLOBAL AND LOCAL INTENSITIES at current step
-        c1, c2, f1, f2  = intensities(I, previousC, sigma, epsilon)
-        GIF_Force = omega*(-l1*(I-c1*np.ones(I.shape))*(I-c1*np.ones(I.shape))\
-                       + l2 * (I - c2*np.ones(I.shape))*(I - c2*np.ones(I.shape)))
-        LIF_Force = np.zeros(I.shape)
-
-        for i in range(previousC.shape[0]):
-            #TEMPORARY : to follow what is going on
-            print('step=',step)
-            percent1 = i/previousC.shape[0]*100
-            print('i=', i, percent1)
-            #
-            for j in range(previousC.shape[1]):             
-                   
-                K_ij = gaussianKernel(I, [i,j], sigma)
-                I_f1_square_ij = ((I[i,j]*np.ones(I.shape))-f1)*((I[i,j]*np.ones(I.shape))-f1)
-                I_f2_square_ij = ((I[i,j]*np.ones(I.shape))-f2)*((I[i,j]*np.ones(I.shape))-f2)
-                LIF_Force[i,j] = -l1*np.sum((K_ij*I_f1_square_ij))+l2*np.sum((K_ij*I_f2_square_ij))
-                LIF_Force[i,j] = (1-omega)*LIF_Force[i,j]
-                
-                dirac[i,j] = epsilon / (m.pi*(epsilon**2+previousC[i,j]**2))
-        
-        #time derivative approximation
-        d_phi = dirac*((1 - omega)*LIF_Force+omega*GIF_Force)\
-        +nu*dirac*div + mu*(lap - div)
-        
-        #what happens at borders ? temporary solution-> enforce no variations
-        d_phi[0:1,:]=np.zeros((1,d_phi.shape[1]))
-        d_phi[d_phi.shape[0]-1:d_phi.shape[0],:]=np.zeros((1,d_phi.shape[1]))
-        d_phi[:,0:1]=np.zeros((d_phi.shape[0],1))
-        d_phi[:,d_phi.shape[1]-1:d_phi.shape[1]]=np.zeros((d_phi.shape[0],1))
-        
-        #New contour 
-        newC = d_phi*dt + previousC
-                         
-        #condition re-calculated for while loop
-        test = np.max(np.absolute(d_phi*dt)) #is there a better test ?
-        print('test=',test)
-        
-        #replace previous contour by new one
-        previousC = newC
+        stop_criterion = np.max(abs(dPhi * dt))
+        previousPhi = newPhi
+    
         step = step+1
-    print(step, test)
-    return previousC
+    
+    return previousPhi
+
+def extractContour(levelSet, image):
+    listC=[]
+    I = image
+    for x in range(1,levelSet.shape[0]-1):
+        for y in range(1,levelSet.shape[1]-1):
+            if levelSet[x,y]>0:
+                if (levelSet[x+1,y]<0 or levelSet[x+1,y-1]<0\
+                or levelSet[x+1,y+1]<0 or levelSet[x-1,y]<0\
+                or levelSet[x-1,y-1]<0 or levelSet[x-1,y+1]<0\
+                or levelSet[x,y-1]<0 or levelSet[x,y+1]<0):
+                    listC.append((x,y))
+                    I[x,y,:] = [0,255,0]
+                
+            elif levelSet[x,y]<0:
+                if (levelSet[x+1,y]>0 or levelSet[x+1,y-1]>0\
+                or levelSet[x+1,y+1]>0 or levelSet[x-1,y]>0\
+                or levelSet[x-1,y-1]>0 or levelSet[x-1,y+1]>0\
+                or levelSet[x,y-1]>0 or levelSet[x,y+1]>0):
+                    listC.append((x,y))
+                    I[x,y,:] = [0,255,0]
+                    
+            else:
+                listC.append((x,y))
+                I[x,y,:] = [0,255,0]
+    
+    return I, listC    
