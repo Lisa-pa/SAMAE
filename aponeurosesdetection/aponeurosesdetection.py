59c97951bc979627f76dd1fd180daf4670e7b098
Lisa-pa <65156893+Lisa-pa@users.noreply.github.com>
2020-07-21 11:19:12 +0200

    Update docstrings

--- aponeurosesdetection/aponeurosesdetection.py
+++ aponeurosesdetection/aponeurosesdetection.py
@@ -57,11 +57,29 @@ def d2_gaussianMasks(s):
 
 def MVEF_2D(I, scales, thresholds):
     """Multiscale Vessel Enhancement Method for 2D images - based on Frangi's,
-    Rana's, and Jalborg's works.
-    I is a grayscale image. thresholds is a list of two thresholds that 
-    control the sensitivity of the line filter to the measures Fr and R.  
-    scales is a list of lengths that correspond to the diameter of the 
-    tube-like structure the operator wants to find"""
+    Rana's, and Jalborg's works. This method searches forr geometrical 
+    structures which can be regarded as tubular.
+    
+    Args:
+        I (2D array):       I is a grayscale image
+        thresholds (list):  thresholds is a list of two thresholds that 
+                            control the sensitivity of the line filter 
+                            to the measures Fr and R.  
+        scales (list):      scales is a list of lengths that correspond to 
+                            the diameter of the tube-like to find
+    
+    Outputs:
+        I2 (2D array):  image I filtered by the multiscale vessel enhancement
+                        filter
+    References:
+        Based on [ Automatic detection of skeletal muscle architecture 
+        features, Frida Elen Jalborg, Master’s Thesis Spring 2016 ]
+                [ Rana et al., 2009, Automated tracking of muscle fascicle
+                orientation in B-mode ultrasound images]
+                [ Rana et al., 2011, In-vivo determination of 3D muscle
+                architecture of human muscle using free hand ultrasound]
+
+    """
     
     vesselness = np.zeros((I.shape[0], I.shape[1], len(scales)))
     I2 = np.zeros((I.shape[0], I.shape[1]))
@@ -123,10 +141,10 @@ def MVEF_2D(I, scales, thresholds):
         for ind2 in range(I2.shape[1]):
             I2[ind1,ind2] = np.max(vesselness[ind1,ind2,:])
 
-    return I2, vesselness, hessian_xy, hessian_xx, hessian_yy
+    return I2
 '-----------------------------------------------------------------------------'
 def apoLocation(I, thresh):
-    """Function that does segmentation on radon transform of image I
+    """Function that segments the radon transform of image I
     to detect the two aponeuroses as the two largest white lines.
     It returns the inverse transform of the segmented radon transform as
     well as location of two horizontal bands containing aponeuroses
