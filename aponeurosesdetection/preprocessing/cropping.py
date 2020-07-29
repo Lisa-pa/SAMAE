d8aea8b97d360e7fa74ca0ca9eb29eec16325647
Lisa-pa <65156893+Lisa-pa@users.noreply.github.com>
2020-07-29 11:23:29 +0200

    Add option to remove skin in US image

--- aponeurosesdetection/preprocessing/cropping.py
+++ aponeurosesdetection/preprocessing/cropping.py
@@ -1,22 +1,27 @@
 import cv2
 import numpy as np
 
-def autocropping(I, thresholdCol_low, thresholdCol_high, thresholdRaw_low, thresholdRaw_high):
+def autocropping(I, threshCmin, threshCmax, threshRmin, threshRmax, calibV = 0):
     """ Cropping of raw ultrasound image I to get region of interest: Removal 
     of lateral, top and bottom strips by thresholding the mean pixel value 
-    of raws and columns. 
+    of raws and columns.
+    If calibV is not zero, then 2 mm are removed from the top of I (this 
+    program assumes that it corresponds to the skin location).
     
     Args:
         I (array): grayscale or RGB image (the function will convert it to 
         grayscale image if it has not yet been done)
-        thresholdCol_low (double): between 0. and 255., minimum threshold value 
+        threshCmin (double): between 0. and 255., minimum threshold value 
         for the lateral strips
-        thresholdCol_high (double): between 0. and 255., maximum threshold value 
+        threshCmax (double): between 0. and 255., maximum threshold value 
         for the lateral strips
-        thresholdRaw_low (double): between 0. and 255., minimum threshold value 
+        threshRmin (double): between 0. and 255., minimum threshold value 
         for the bottom and top strips
-        thresholdRaw_high (double): between 0. and 255., maximum threshold value 
+        threshRmax (double): between 0. and 255., maximum threshold value 
         for the bottom and top strips
+        calibV (double): vertical calibration factor. Default value is 0.
+        If calibV is different from 0, then the equivalent of 2mm is
+        cropped at the top of I to remove skin from the image.
 
     Returns:
         I2 (array): same dimension as I, containing only the region of interest
@@ -46,30 +51,34 @@ def autocropping(I, thresholdCol_low, thresholdCol_high, thresholdRaw_low, thres
     BottomRawsBelow = [0]
     
     #vertical cropping
-    while thresholdCol_low < thresholdCol_high and \
+    while threshCmin < threshCmax and \
     (len(LeftColumnsBelow)==1 or len(RightColumnsBelow)==1):
         
         for col in range(int(grayI.shape[1]/2)):
-            if np.mean(grayI[:,col])<=thresholdCol_low:
+            if np.mean(grayI[:,col])<=threshCmin:
                 RightColumnsBelow.append(col)
-            if np.mean(grayI[:,grayI.shape[1]-1-col])<=thresholdCol_low:
+            if np.mean(grayI[:,grayI.shape[1]-1-col])<=threshCmin:
                 LeftColumnsBelow.append(grayI.shape[1]-1-col)
-        thresholdCol_low+=1.
+        threshCmin+=1.
         
     #horizontal cropping
-    while thresholdRaw_low < thresholdRaw_high and \
+    while threshRmin < threshRmax and \
     (len(UpRawsBelow)==1 or len(BottomRawsBelow)==1):
         
         for lig in range(int(grayI.shape[0]/2)):
-            if np.mean(grayI[lig,:])<=thresholdRaw_low:
+            if np.mean(grayI[lig,:])<=threshRmin:
                 UpRawsBelow.append(lig)
-            if np.mean(grayI[grayI.shape[0]-1-lig,:])<=thresholdRaw_low:
+            if np.mean(grayI[grayI.shape[0]-1-lig,:])<=threshRmin:
                 BottomRawsBelow.append(grayI.shape[0]-1-lig)
-        thresholdRaw_low+=2.  
+        threshRmin+=2.  
     
     if len(I.shape) == 3:
         I2 = I[UpRawsBelow[-1]:BottomRawsBelow[-1],RightColumnsBelow[-1]:LeftColumnsBelow[-1],:]
     elif len(I.shape) == 2:
         I2 = I[UpRawsBelow[-1]:BottomRawsBelow[-1],RightColumnsBelow[-1]:LeftColumnsBelow[-1]]
     
+    if calibV != 0:
+        twomm = int(2 / calibV)
+        I2 = I2[twomm:,:]
+        
     return I2
