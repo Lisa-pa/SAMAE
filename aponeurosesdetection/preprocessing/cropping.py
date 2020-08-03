2d4f69ac1474fa418d7e10d000511fe7b9efed44
Lisa-pa <65156893+Lisa-pa@users.noreply.github.com>
2020-08-03 14:44:01 +0200

    Create fction to crop thks to manually picked pts

--- aponeurosesdetection/preprocessing/cropping.py
+++ aponeurosesdetection/preprocessing/cropping.py
@@ -82,3 +82,72 @@ def autocropping(I, threshCmin, threshCmax, threshRmin, threshRmax, calibV = 0):
         I2 = I2[twomm:,:]
         
     return I2
+
+def manualcropping(I, pointsfile):
+    """This function crops a copy of image I according to points stored 
+    in a text file (pointsfile) and corresponding to aponeuroses (see 
+    Args section).
+
+    Args:
+        I (array): 3-canal image
+        pointsfile (text file): contains points' coordinates. Pointsfile must be 
+        organized such that:
+            - column 0 is the ID of each point
+            - column 1 is the X coordinate of each point, ie the corresponding 
+            column in I
+            - column 2 is the Y coordinate, ie the raw in I
+            - raw 0 is for columns' name
+            - raws 1 and 2 are for two points of the scale
+            - raws 3 to 13 are aponeuroses' points in panoramic images // raws 3 
+            to 10 in simple images
+            - following raws are for muscle fascicles (and are optional for this 
+            function)
+            pointsfile's name must 1) include extension 2) indicates whether I
+            is panoramic or simple by having 'p' or 's' just before the point
+            of the extension.
+
+        Returns:
+            array of same type than I. It is the cropped image of I according
+            to the aponeuroses' points manually picked and stored in pointsfile.
+    """
+    
+    data = open(pointsfile, 'r')
+    
+    #finds whether the image is panoramic or simple
+    searchpoint = -1
+    while (pointsfile[searchpoint] != '.') and (searchpoint>(-len(pointsfile))):
+        searchpoint = searchpoint-1
+    if (searchpoint == -len(pointsfile)):
+        raise TypeError('Input pointsfile name is not correct. Check extension.')
+    else:
+        imagetype = pointsfile[searchpoint-1]
+    
+    #extract points from the input file
+    pickedPoints= []
+    for line in data:
+        line = line.strip('\n')
+        x = line.split('\t')
+        pickedPoints.append((x[1],x[2]))
+
+    #keep aponeuroses points according to image type
+    if imagetype == 'p': #keep points 4 to 14 included
+        apos = np.asarray(pickedPoints[3:14], dtype=np.float64, order='C')
+        print(apos)
+    elif imagetype == 's': #keep points 4 to 11 included
+        apos = np.asarray(pickedPoints[3:11], dtype=np.float64, order='C')
+    else:
+        raise ValueError("pointsfile's name does not fulfill conditions. See docstrings")
+
+    #find max and min indexes for columns and raws to crop image I
+    minRaw = max(0, min(apos[:,1])-5)
+    maxRaw = min(I.shape[0], max(apos[:,1])+10)
+    minCol = max(0, np.min(apos[:,0])-10)
+    maxCol = min(I.shape[1], max(apos[:,0])+10)
+
+    I2 = np.copy(I)
+    I2 = I2[int(minRaw):int(maxRaw),int(minCol):int(maxCol),:]
+
+    #close file
+    data.close()
+    
+    return I2
