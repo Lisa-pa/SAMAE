904de30000ca05780252a343b1fe457ca44db96d
Lisa-pa <65156893+Lisa-pa@users.noreply.github.com>
2020-09-03 15:38:22 +0200

    just began adjustments

--- aponeurosesdetection/filemanager.py
+++ aponeurosesdetection/filemanager.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
-from aponeurosesdetection.arch import *
+from arch import *
 
 # manually change the path or ask for a user folder selection input
-path_to_folders = str(Path(r'C:\Users\Antonio\Dropbox\Paillard_Lisa\jHamON_data'))
+# path_to_folders = str(Path(r'C:\Users\Antonio\Dropbox\Paillard_Lisa\jHamON_data'))
+path_to_folders = str(Path(r'C:\Users\Lisa Paillard\Desktop\Pour TFE\jHamON_data'))
 
 
 archpaths = dame_arch_paths(path_to_folders=path_to_folders)
@@ -12,3 +13,7 @@ arch_dict = dame_arch_data(archpaths)
 
 # # example of nested dicts 
 # arch_dict['01_Kevin']['fam_1']['BF']['simple']['img_1']['architecture']['fsc_1'].keys()
+
+
+#if path leads to panoramic image -> execute main_panoramic
+#elif path leads to simple image -> execute main_simple
