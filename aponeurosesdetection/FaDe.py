46d9ac9704ca7d4f0d5212f1dd444c317600562a
Lisa-pa <65156893+Lisa-pa@users.noreply.github.com>
2020-09-30 10:54:33 +0200

    Change snippets location strategy

--- aponeurosesdetection/FaDe.py
+++ aponeurosesdetection/FaDe.py
@@ -123,52 +123,8 @@ def MVEF_2D(I, scales, thresholds):
     
     return I2
 '-----------------------------------------------------------------------------'
-"""
-from skimage.transform import radon, iradon
-def normalizedRadon(I):
 
-
-    if len(I.shape) > 2:
-        I = cv2.cvtColor(I, cv2.COLOR_RGB2GRAY)
-    
-    #working with a square because radon function working on a circle only
-    if I.shape[0] > I.shape[1]:
-        I = I[int(I.shape[0]/2 - I.shape[1]/2):int(I.shape[0]/2 + I.shape[1]/2), :]
-    else:
-        I = I[:, int(I.shape[1]/2 - I.shape[0]/2):int(I.shape[1]/2 + I.shape[0]/2)]
-
-    cv2.imshow('Part of the image analyzed', I)
-    cv2.waitKey(0) & 0xFF
-    cv2.destroyAllWindows()
-
-    R = radon(I, circle = True)
-    cv2.imshow('Radon Transform', R/np.max(R))
-    cv2.waitKey(0) & 0xFF
-    cv2.destroyAllWindows()
-    
-    X = np.arange(0,I.shape[0],1)
-    Y = np.arange(0,I.shape[1],1)
-
-    for rho in range(R.shape[0]):
-        for theta in range(R.shape[1]):
-            line = rho - m.cos(theta)*X + m.sin(theta)*Y
-            #line = (np.abs(rho - m.cos(theta)*X + m.sin(theta)*Y) <0.6)*1. + (np.abs(rho - m.cos(theta)*X + m.sin(theta)*Y) >= 0.6)*0.
-            dirac = 1./(m.pi*(1.*1.+line*line))        
-
-            L = np.sum(dirac)
-            if L != 0:
-                R[rho, theta] = R[rho, theta]/L
-
-    cv2.imshow('Radon Transform', R/np.max(R))
-    cv2.waitKey(0) & 0xFF
-    cv2.destroyAllWindows()
-    
-    R = cv2.threshold(R, 254., 255, cv2.THRESH_BINARY)[1]
-
-    return R,I
-"""
-
-def locateSnippets(I, xcalib, ycalib, minLength, rangeAngles, percentageAlign, offSetX = 0, offSetY = 0):
+def locateSnippets(I, xcalib, ycalib, minLength, offSetX = 0, offSetY = 0, im = None):
     """
     I (array): binary image (one canal)
     """
@@ -178,86 +134,107 @@ def locateSnippets(I, xcalib, ycalib, minLength, rangeAngles, percentageAlign, o
     if len(snippets) == 0:
         return 'error', 'error'
     
-    filtered_indices = [] # list which contains the ID of the filtered snippets
-    line_snip = [] #list which contains a linear approximation of the snippet filtered
+    line_snip = [] #list which contains a linear approximation of the snippet sn
+    
+    vect = np.zeros((len(snippets), 4))
     
     for i in range(len(snippets)):
         sn = list(snippets[i][:,0,:])
-        #condition 1 - on length :
-        if len(sn)/2 > minLength/m.sqrt(xcalib**2+ycalib**2):
-            
-            
-            # creation of a line representative of the snippet i
-            # firstP and endP are are respectively
-            # in the left part and in the right part of snippet i
-            third = 1/3.*len(sn)/2
-                        
-            sn.sort(key=lambda x:x[0]) #sort according to columns
-            
-            
-            left_points = [sn[i] for i in range(len(sn)) if i == int(third)]
-            right_points = [sn[i] for i in range(len(sn)) if i == len(sn)-int(third)]
-            
-            
-            left_points.sort(key=lambda x:x[1]) #sort according to lines
-            right_points.sort(key = lambda x:x[1])
-            
-            
-            if len(left_points)==2:
-                firstP = [int((left_points[0][0] + left_points[-1][0])/2),\
-                          int((left_points[0][1] + left_points[-1][1])/2)]
-            elif len(left_points) == 1:
-                firstP = left_points[0]
-            elif len(left_points) > 2:
-                firstP = [sn[i] for i in range(len(sn))\
-                          if sn[i][0] == np.amin(snippets[i][:,0,0])][0]
-            
-            
-            if len(right_points)==2:
-                endP = [int((right_points[0][0] + right_points[-1][0])/2),\
-                        int((right_points[0][1] + right_points[-1][1])/2)]
-            elif len(right_points)==1:
-                endP = right_points[0]
-            elif len(left_points) > 2:
-                endP = [sn[i] for i in range(len(sn))\
-                        if sn[i][0] == np.amax(snippets[i][:,0,0])][0]
-                
-            y_list = np.arange(firstP[0], endP[0], 1, dtype = np.int64) #create line (x_list, y_list) between firstP and endP
+        sn.sort(key=lambda x:x[0]) #sort according to columns  
+    
+        #######################################################################
+        # construction of 2 points p1, p2 to represent sn by a line
+        third = 1/3.*len(sn)/2
+        left_points = [sn[i] for i in range(len(sn)) if sn[i][0] == sn[0][0] + int(third)]
+        right_points = [sn[i] for i in range(len(sn)) if sn[i][0] == sn[-1][0] - int(third)]
+        left_points.sort(key=lambda x:x[1]) #sort according to lines
+        right_points.sort(key = lambda x:x[1])
 
+        if len(left_points)==2:
+            p1 = [int((left_points[0][0] + left_points[-1][0])/2),\
+                      int((left_points[0][1] + left_points[-1][1])/2)]
+        elif len(left_points) == 1:
+            p1 = left_points[0]
+        elif len(left_points) > 2: #take the extremal left point of the snippet
+            p1 = [sn[i] for i in range(len(sn))\
+                      if sn[i][0] == np.amin(np.array(sn)[:,0])][0]
+        
+        if len(right_points)==2:
+            p2 = [int((right_points[0][0] + right_points[-1][0])/2),\
+                    int((right_points[0][1] + right_points[-1][1])/2)]
+        elif len(right_points)==1:
+            p2 = right_points[0]
+        elif len(right_points) > 2: #take right extremal point of the snippet
+            p2 = [sn[i] for i in range(len(sn))\
+                    if sn[i][0] == np.amax(np.array(sn)[:,0])][0]
 
-            #condition 2 - on area/length
-                ###this condition is too restrictive
-            """area = cv2.contourArea(snippets[i])
-            if round(area/y_list.shape[0],1) > 5. and round(area/y_list.shape[0],1) < 9.:
-            """   
-            
-            #condition 3 - on the angle with horizontal
-            if endP[0]-firstP[0] != 0:
-                
-                slope = (endP[1]-firstP[1])/(endP[0]-firstP[0])
-                angle = -m.atan(slope)*180/m.pi            
-
-                if angle>rangeAngles[0] and abs(angle)<rangeAngles[1]:
-                    
-                    x_list = np.uint64(slope*(y_list-firstP[0])+firstP[1])  
-                    
-                    ''' visual check
-                    for pt in range(y_list.shape[0]):
-                        I[int(x_list[pt]), int(y_list[pt])] = 150
-                    '''
-                        
-                #condition 4 - on alignment
-                    #count the number of white pixels on this line
-                    pix_white = 0
-                    for n in range(y_list.shape[0]):
-                        if I[x_list[n], y_list[n]] > 0 :
-                            pix_white = pix_white + 1
-                    if pix_white/y_list.shape[0] > percentageAlign: #0.65 for simple images, 0.8 for pano
-
-                        filtered_indices.append(i)
-
-                        line_snip.append([slope, [firstP[0] + offSetY, firstP[1] + offSetX]])
-                            
+        # create line (x_list, y_list) inside snippet
+        cmin = np.amin(snippets[i][:,0,0])
+        cmax = np.amax(snippets[i][:,0,0])
+        y_list = np.arange(cmin, cmax, 1)
+        if p2[0]-p1[0] != 0:
+            slope = (p2[1]-p1[1])/(p2[0]-p1[0])
+            x_list = (slope*(y_list-p1[0])+p1[1])
+            line_snip.append([slope, [p1[0] + offSetY, p1[1] + offSetX]])
+        else:
+            slope = 0
+            x_list = np.arange(np.amin(snippets[i][:,0,1]), np.amax(snippets[i][:,0,1]), 1)
+            line_snip.append([slope, [p1[0] + offSetY, p1[1] + offSetX]])
+        
+        
+        #######################################################################
+        
+        # Features: length, alignement, aspect ratio and angle with horizontal
+        if len(sn) > 5 : #make sure minimum of points required for fitEllipse function is reached
+            (x0,y0),(mia,maa), ang = cv2.fitEllipse(snippets[i])
+            angle = 90 - ang
+            aspra = float(mia/maa)
+            if angle == 90:
+                align = 0
+            else:
+                pix_white = 0
+                for n in range(y_list.shape[0]):
+                    if I[int(x_list[n]), int(y_list[n])] > 0 :
+                        pix_white = pix_white + 1
+                align = pix_white/y_list.shape[0]           
+        else:
+            maa = 0
+            angle = 0
+            aspra = 0.
+            align = 0
+        
+        vect[i,0] = maa * xcalib
+        vect[i,1] = angle
+        vect[i,2] = align
+        vect[i,3] = aspra
+        
+    # Normalization
+    medianL = np.median(vect[:,0])
+    medianAng = np.median(vect[:,1])
+    medianAl = np.median(vect[:,2])
+    medianAR = np.median(vect[:,3])
+    
+    madL = np.median(abs(vect[:,0] - medianL))
+    madAng = np.median(abs(vect[:,1] - medianAng))
+    madAl = np.median(abs(vect[:,2] - medianAl))
+    madAR = np.median(abs(vect[:,3] - medianAR))
+    
+    vect[:,0] = (vect[:,0] - medianL) / madL
+    vect[:,1] = (vect[:,1] - medianAng) / madAng
+    vect[:,2] = (vect[:,2] - medianAl) / madAl
+    vect[:,3] = (vect[:,3] - medianAR) / madAR
+        
+    norm = np.sqrt(vect[:,0]*vect[:,0] + vect[:,1]*vect[:,1]\
+        + vect[:,2]*vect[:,2] + vect[:,3]*vect[:,3])
+   
+    #Filtering on norm
+    lines = []
+    filtered_indices = [] # list which contains the ID of the filtered snippets
+    for n in range(len(snippets)):
+        if norm[n] >= 4 and vect[n,0] > minLength:
+            filtered_indices.append(n)
+            lines.append(line_snip[n])
+    
     #outputs 
     filtered_snippets = []
     for fa in range(len(filtered_indices)):
@@ -266,21 +243,13 @@ def locateSnippets(I, xcalib, ycalib, minLength, rangeAngles, percentageAlign, o
         fasc[:, 0] = fasc[:, 0] + offSetX
         fasc[:,1] = fasc[:, 1] + offSetY
         filtered_snippets.append(fasc)
-        
-    '''visual check
-    cv2.imshow('image', I)
-    cv2.waitKey(0) & 0xFF
-    cv2.destroyAllWindows()
-    '''
     
-    return filtered_snippets, line_snip
+    return filtered_snippets, lines
 
 
 
 
-
-
-def combineSnippets(I, listS, listS_paramlines, min_nb_sn, thresh_alignment = 20, thresh_length = 200):
+def combineSnippets(I, listS, listS_paramlines, min_nb_sn, thresh_alignment = 20):
     """listS contient les snippets en mode [x,y]"""
     #Look for snippets that are part of the same fascicles
 
@@ -292,39 +261,43 @@ def combineSnippets(I, listS, listS_paramlines, min_nb_sn, thresh_alignment = 20
         for s in range(len(listS) - 1):
             ylist_s = np.arange(0, I.shape[1],1, dtype = np.int64)
             slope_s = listS_paramlines[s][0] #slope
-            firstP_s = listS_paramlines[s][1] #point on the line of the snippet s
-            xlist_s = (slope_s*(ylist_s-firstP_s[0])+firstP_s[1])
+            p1_s = listS_paramlines[s][1] #point on the line of the snippet s = [y, x]
+            xlist_s = (slope_s*(ylist_s-p1_s[0])+p1_s[1])
             xy_list_s = np.uint64(np.vstack((xlist_s, ylist_s))).T
-                        
+
             for n in range(s+1, len(listS)):
                 #check if the previous line intersect a snippet n different from s
                 
                 slope_n = listS_paramlines[n][0]
-                firstP_n = listS_paramlines[n][1]
+                p1_n = listS_paramlines[n][1]
                 ylist_n = np.arange(0, I.shape[1],1, dtype = np.int64)
-                xlist_n = (slope_n*(ylist_n-firstP_n[0])+firstP_n[1])
+                xlist_n = (slope_n*(ylist_n-p1_n[0])+p1_n[1])
                 xy_list_n = np.uint64(np.vstack((xlist_n, ylist_n))).T
                 
                 intersection1 = set.intersection(set(map(tuple,xy_list_s)), set(map(tuple, listS[n])))
                 intersection2 = set.intersection(set(map(tuple,xy_list_n)), set(map(tuple, listS[s])))
                 
-                #if intersection not empty and if first points of snippets roughly aligned : 
-                if (intersection1 or intersection2) and\
-                abs(firstP_n[1]-xlist_s[firstP_n[0]])<thresh_alignment and\
-                abs(firstP_s[1]-xlist_n[firstP_s[0]])<thresh_alignment:
-                    
-                    # if snippets not too far from each other 
-                    # ie maximum a quarter of I's length
-                    if np.amin(np.array(listS[n])[:,1]) < np.amin(np.array(listS[s])[:,1]): 
-                        if abs(np.amax(np.array(listS[n])[:,1])\
-                               - np.amin(np.array(listS[s])[:,1])) < I.shape[1]/5:
-                            aligned_snip[s].append(n)
+                
+                #if intersection not empty  
+                if (intersection1 or intersection2):
+
+                    if np.amin(np.array(listS[n])[:,1]) < np.amin(np.array(listS[s])[:,1]):
+                        # if snippets roughly aligned :
+                        if abs(p1_s[1]-xlist_n[p1_s[0]])<thresh_alignment:
+                            # if snippets not too far from each other 
+                            # ie maximum a quarter of I's length
+                            if abs(np.amax(np.array(listS[n])[:,1])\
+                               - np.amin(np.array(listS[s])[:,1])) < I.shape[1]/4:
+                                aligned_snip[s].append(n)
                                                                
                     elif np.amin(np.array(listS[n])[:,1]) >= np.amin(np.array(listS[s])[:,1]):
-                        if abs(np.amin(np.array(listS[n])[:,1])\
-                               - np.amax(np.array(listS[s])[:,1])) < I.shape[1]/5:
-                            aligned_snip[s].append(n)
-
+                        # if snippets roughly aligned :
+                        if abs(p1_n[1]-xlist_s[p1_n[0]])<thresh_alignment:
+                            # if snippets not too far from each other 
+                            # ie maximum a quarter of I's length                
+                            if abs(np.amin(np.array(listS[n])[:,1])\
+                                   - np.amax(np.array(listS[s])[:,1])) < I.shape[1]/4:
+                                aligned_snip[s].append(n)
 
         #example : we obtain aligned_snip = [[1,2,3],[2,5],[3],[4,5],[5],[7],[8,10],[10]]
         # we need to merge [1,2,3], [2,5], [3], [4,5] and [5]; and we need to merge
@@ -340,19 +313,9 @@ def combineSnippets(I, listS, listS_paramlines, min_nb_sn, thresh_alignment = 20
                     test = 1
             
             if test == 0:
-                #if more than 2 snippets in aligned_snip[f], consider it in grouped_snip:
-                
-                if len(aligned_snip[f]) >= 2:
-                    grouped_snip.append(aligned_snip[f])
+                grouped_snip.append(aligned_snip[f])
                     
-                # if there is only 1 snippet in aligned_snip[f], check if it
-                # is long enough
-                
-                if len(aligned_snip[f]) == 1 and len(listS[aligned_snip[f][0]]) > thresh_length:
-                    grouped_snip.append(aligned_snip[f])
-
-        print('final merged fascicles', grouped_snip)
-        
+       
         #outputs 
         for fa in range(len(grouped_snip)):
             fasc = listS[grouped_snip[fa][0]]
@@ -362,18 +325,7 @@ def combineSnippets(I, listS, listS_paramlines, min_nb_sn, thresh_alignment = 20
                 for s in range(1,len(grouped_snip[fa])):
                     fasc = np.concatenate((fasc, listS[grouped_snip[fa][s]]), axis = 0)
                 fascicles_points.append(fasc)
-
-
-    """
-    couleurs = [[255,0,0], [0,255,0], [0,0,255], [255,255,0],[255,0,255], [0,255,255],\
-                [255,255,255], [100,200,0],[100,200,100], [50,200,0],[50,100,50], [255,100,0],\
-                [120,120,255], [255,80,80],[0,100,200], [0,100,80]]
-    for t in range(len(fascicles_points)):
-        for k in range(fascicles_points[t].shape[0]):
-            pt = fascicles_points[t][k,:]
-            I[pt[0], pt[1], :] = couleurs[t]
-    """
-
+    
     return fascicles_points, fascicles2_points
 
 
