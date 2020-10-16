0a5eedc23e762fd5bbe72e3af786da7ed0131a60
Lisa-pa <65156893+Lisa-pa@users.noreply.github.com>
2020-10-16 09:57:22 +0200

    adapt to dictionnary architecture

--- aponeurosesdetection/manuP.py
+++ aponeurosesdetection/manuP.py
@@ -15,52 +15,51 @@ def panoManu(manu_pts_dict, path_img = None):
     import scipy.interpolate as interpolate
 
     calib = manu_pts_dict['calfct_to_mm']
-    pt_inter = [manu_pts_dict['insertion']['coords'][1], manu_pts_dict['insertion']['coords'][0]]
-
+    pt_inter = manu_pts_dict['insertion']['coords']
 
     #aponeuroses interpolation with Bspline
     list_sup = list(manu_pts_dict['aposup']['coords'])
-    list_sup.sort(key=lambda x:x[0])
-    y_sup = [list_sup[i][0] for i in range(len(list_sup))]
-    x_sup = [list_sup[i][1] for i in range(len(list_sup))]
+    list_sup.sort(key=lambda x:x[1])
+    y_sup = [list_sup[i][1] for i in range(len(list_sup))]
+    x_sup = [list_sup[i][0] for i in range(len(list_sup))]
     
     list_inf = list(manu_pts_dict['apoinf']['coords'])
-    list_inf.sort(key=lambda x:x[0])
-    y_inf = [list_inf[i][0] for i in range(len(list_inf))]
-    x_inf = [list_inf[i][1] for i in range(len(list_inf))]
-
-    spline_sup = interpolate.UnivariateSpline(y_sup, x_sup, k=1, ext = 0) 
+    list_inf.sort(key=lambda x:x[1])
+    y_inf = [list_inf[i][1] for i in range(len(list_inf))]
+    x_inf = [list_inf[i][0] for i in range(len(list_inf))]
+    
+    spline_sup = interpolate.UnivariateSpline(y_sup, x_sup, k=1, ext = 0)
     spline_inf = interpolate.UnivariateSpline(y_inf, x_inf, k=1, ext = 0)
     coordSup, spline_sup = MUFeaM.pointsCoordinates(typeA = 'spline', param = spline_sup, interval = [0, int(pt_inter[1])])
     coordInf, spline_inf = MUFeaM.pointsCoordinates(typeA = 'spline', param = spline_inf, interval = [0, int(pt_inter[1])])
 
 
-
     # features computation
     abscissa, thickness, splineT = MUFeaM.muscleThickness(start = max(min(y_sup), min(y_inf)), end = min(max(y_sup), max(y_inf)), calibV = calib, calibH = calib, spl1 = spline_sup, spl2 = spline_inf)
-    manu_pts_dict['MT'] = {'coords': np.vstack((abscissa, thickness)).T, 'columns interval': [max(min(y_sup), min(y_inf)), min(max(y_sup), max(y_inf))]}
+    manu_pts_dict['MT'] = {'coords': thickness, 'columns interval': [max(min(y_sup), min(y_inf)), min(max(y_sup), max(y_inf))]}
     
     for fsc in fnmatch.filter(manu_pts_dict.keys(), 'fsc_*'):
         fasci = manu_pts_dict[fsc]['coords']        
-        fasci[:, [0,1]] = fasci[:,[1,0]] #change to structure (X,Y)        
         
         #Bspline
         spl = FaDe.approximateFasc(typeapprox = 'Bspline', listF = [fasci], d = 1)
         #find intersection points with aponeuroses
-        inters_inf, inters_sup, spl = MUFeaM.findIntersections(spl_inf = spline_inf, spl_sup = spline_sup, listSpl = spl, start = pt_inter[1]/2, insertion= pt_inter[1])
-        #find pennation angles
-        PA_sup = MUFeaM.pennationAngles(spl_a = spline_sup, listS_f = spl, listI = inters_sup, xcalib = calib, ycalib =  calib)
-        PA_inf = MUFeaM.pennationAngles(spl_a = spline_inf, listS_f = spl, listI = inters_inf, xcalib = calib, ycalib = calib)
-        #calculate fascicle length
-        FL = MUFeaM.fasciclesLength(listS_f = spl, listIu = inters_sup, listId = inters_inf, xcalib = calib, ycalib = calib)
-        loc = MUFeaM.locateFasc(inters_inf, refPoint = pt_inter, ycalib = calib)
-        
-        manu_pts_dict[fsc]['dist from insertion in mm'] = loc[0]
-        manu_pts_dict[fsc]['PAsup'] = {'value in degree' : PA_sup[0],
-                     'intersection with apo': inters_sup[0]}
-        manu_pts_dict[fsc]['PAinf'] = {'value in degree' : PA_inf[0],
-                     'intersection with apo': inters_inf[0]}
-        manu_pts_dict[fsc]['FL'] = {'length in mm': FL[0]}
+        inters_inf, inters_sup, spl = MUFeaM.findIntersections(spl_inf = spline_inf, spl_sup = spline_sup,\
+                                                               listSpl = spl, search_interval = [0, pt_inter[1]], start=0)
+        if len(spl)>0:
+            #find pennation angles
+            PA_sup = MUFeaM.pennationAngles(spl_a = spline_sup, listS_f = spl, listI = inters_sup, xcalib = calib, ycalib =  calib)
+            PA_inf = MUFeaM.pennationAngles(spl_a = spline_inf, listS_f = spl, listI = inters_inf, xcalib = calib, ycalib = calib)
+            #calculate fascicle length
+            FL = MUFeaM.fasciclesLength(listS_f = spl, listIu = inters_sup, listId = inters_inf, xcalib = calib, ycalib = calib)
+            loc = MUFeaM.locateFasc(inters_inf, refPoint = pt_inter, ycalib = calib)
+            
+            manu_pts_dict[fsc]['dist from insertion in mm'] = loc[0]
+            manu_pts_dict[fsc]['PAsup'] = {'value in degree' : PA_sup[0],
+                         'intersection with apo': inters_sup[0]}
+            manu_pts_dict[fsc]['PAinf'] = {'value in degree' : PA_inf[0],
+                         'intersection with apo': inters_inf[0]}
+            manu_pts_dict[fsc]['FL'] = {'length in mm': FL[0]}
 
         if path_img is not None: 
             import cv2
@@ -92,15 +91,7 @@ def panoManu(manu_pts_dict, path_img = None):
                     I[coordInf[index][0]+1, coordInf[index][1], :] = [255, 0, 0]
                 if coordInf[index][0]-1 >= 0 and coordInf[index][0]-1 < I.shape[0]:
                     I[coordInf[index][0]-1, coordInf[index][1], :] = [255, 0, 0]
-    
-        # intersection point visualization
-        I[int(pt_inter[0]), int(pt_inter[1]),:] = [255,255,0]
-        I[int(pt_inter[0])-1, int(pt_inter[1]),:] = [255,255,0]
-        I[int(pt_inter[0])+1, int(pt_inter[1]),:] = [255,255,0]
-        I[int(pt_inter[0]), int(pt_inter[1])-1,:] = [255,255,0]
-        I[int(pt_inter[0]), int(pt_inter[1])+1,:] = [255,255,0]
-    
-        
+
         cv2.imshow('full image', I)
         cv2.waitKey(0) & 0xFF
         cv2.destroyAllWindows()
--- aponeurosesdetection/manuS.py
+++ aponeurosesdetection/manuS.py
@@ -15,14 +15,14 @@ def simpleManu(manu_pts_dict, path_img = None):
     calib = manu_pts_dict['calfct_to_mm']
     
     list_sup = list(manu_pts_dict['aposup']['coords'])
-    list_sup.sort(key=lambda x:x[0])
-    y_sup = [list_sup[i][0] for i in range(len(list_sup))]
-    x_sup = [list_sup[i][1] for i in range(len(list_sup))]
+    list_sup.sort(key=lambda x:x[1])
+    y_sup = [list_sup[i][1] for i in range(len(list_sup))]
+    x_sup = [list_sup[i][0] for i in range(len(list_sup))]
     
     list_inf = list(manu_pts_dict['apoinf']['coords'])
-    list_inf.sort(key=lambda x:x[0])
-    y_inf = [list_inf[i][0] for i in range(len(list_inf))]
-    x_inf = [list_inf[i][1] for i in range(len(list_inf))]
+    list_inf.sort(key=lambda x:x[1])
+    y_inf = [list_inf[i][1] for i in range(len(list_inf))]
+    x_inf = [list_inf[i][0] for i in range(len(list_inf))]
 
     #interpolation with Bspline        
     polyn_sup = np.polyfit(y_sup, x_sup, deg = 1)
@@ -32,28 +32,29 @@ def simpleManu(manu_pts_dict, path_img = None):
     
     # features computation
     abscissa, thickness, splineT = MUFeaM.muscleThickness(start = max(min(y_sup), min(y_inf)), end = min(max(y_sup), max(y_inf)), calibV = calib, calibH = calib, spl1 = spline_sup, spl2 = spline_inf)
-    manu_pts_dict['MT'] = {'coords': np.vstack((abscissa, thickness)).T, 'columns interval': [max(min(y_sup), min(y_inf)), min(max(y_sup), max(y_inf))]}
+    manu_pts_dict['MT'] = {'coords': thickness, 'columns interval': [max(min(y_sup), min(y_inf)), min(max(y_sup), max(y_inf))]}
     
     for fsc in fnmatch.filter(manu_pts_dict.keys(), 'fsc_*'):
         fasci = manu_pts_dict[fsc]['coords']        
-        fasci[:, [0,1]] = fasci[:,[1,0]] #change to structure (X,Y)
 
         #Bspline
         spl = FaDe.approximateFasc(typeapprox = 'polyfit', listF = [fasci], d = 1)
         #find intersection points with aponeuroses
-        inters_inf, inters_sup, spl = MUFeaM.findIntersections(spl_inf = spline_inf, spl_sup = spline_sup, listSpl = spl, start = 0, insertion = 200/calib)
-        #find pennation angles
-        PA_sup = MUFeaM.pennationAngles(spl_a = spline_sup, listS_f = spl, listI = inters_sup, xcalib = calib, ycalib = calib)
-        PA_inf = MUFeaM.pennationAngles(spl_a = spline_inf, listS_f = spl, listI = inters_inf, xcalib = calib, ycalib = calib)
-        #calculate fascicle length
-        FL = MUFeaM.fasciclesLength(listS_f = spl, listIu = inters_sup, listId = inters_inf, xcalib = calib, ycalib = calib)
-        loc = MUFeaM.locateFasc(inters_inf, [0,0], calib)
-        
-        manu_pts_dict[fsc]['dist from (0,0) of RGB image, in mm'] = loc[0]
-        manu_pts_dict[fsc]['PAsup'] = {'value in degree' : PA_sup[0],
-                     'intersection with apo': inters_sup[0]}
-        manu_pts_dict[fsc]['PAinf'] = {'value in degree' : PA_inf[0],
-                     'intersection with apo': inters_inf[0]}
-        manu_pts_dict[fsc]['FL'] = {'length in mm': FL[0]}
+        inters_inf, inters_sup, spl = MUFeaM.findIntersections(spl_inf = spline_inf, spl_sup = spline_sup,\
+                                                               listSpl = spl, search_interval = [-200/calib,200/calib], start = 0)
+        if len(spl)>0:
+            #find pennation angles
+            PA_sup = MUFeaM.pennationAngles(spl_a = spline_sup, listS_f = spl, listI = inters_sup, xcalib = calib, ycalib = calib)
+            PA_inf = MUFeaM.pennationAngles(spl_a = spline_inf, listS_f = spl, listI = inters_inf, xcalib = calib, ycalib = calib)
+            #calculate fascicle length
+            FL = MUFeaM.fasciclesLength(listS_f = spl, listIu = inters_sup, listId = inters_inf, xcalib = calib, ycalib = calib)
+            loc = MUFeaM.locateFasc(inters_inf, [0,0], calib)
+            
+            manu_pts_dict[fsc]['dist from (0,0) of RGB image, in mm'] = loc[0]
+            manu_pts_dict[fsc]['PAsup'] = {'value in degree' : PA_sup[0],
+                         'intersection with apo': inters_sup[0]}
+            manu_pts_dict[fsc]['PAinf'] = {'value in degree' : PA_inf[0],
+                         'intersection with apo': inters_inf[0]}
+            manu_pts_dict[fsc]['FL'] = {'length in mm': FL[0]}
    
     return manu_pts_dict
