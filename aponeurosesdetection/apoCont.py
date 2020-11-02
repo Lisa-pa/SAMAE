6916423d7324f6dd1b88b5c96b371712c4ec486b
Lisa-pa <65156893+Lisa-pa@users.noreply.github.com>
2020-11-02 13:19:51 +0100

    update docstrings and comments

--- aponeurosesdetection/apoCont.py
+++ aponeurosesdetection/apoCont.py
@@ -12,7 +12,7 @@ def gaussianKernel(sigma):
         sigma (double): standard deviation
 
     Returns:
-        numpy array: 2D array of size (4*sigma+1)x(4*sigma+1).
+        numpy array: kernel as a 2D array of size (4*sigma+1)x(4*sigma+1).
     """
     sizeK = 4 * int(sigma) + 1
     a = np.ogrid[-int(sizeK/2):int(sizeK/2)+1, -int(sizeK/2):int(sizeK/2)+1][0]*np.ones((sizeK, sizeK))
@@ -37,14 +37,14 @@ def intensities(I, previousPhi, eps, s, l1, l2):
     Args:
         I (numpy array): 2D image
         previousPhi (numpy array): 2D matrix of a level set function that
-        modelizes a closed contour - See references
-        eps (double): parameter used in dirac and heaviside functions approximation
+            modelizes a closed contour - We advice you to read the references
+        eps (double): parameter used in dirac and heaviside 
+            functions approximation, stricly positive
         s (int): standard deviation of gaussian kernels
-        epsilon (double): strictly positive constant
         l1, l2 (double): strictly positive constants 
 
     Returns:
-        tuple: (c1,c2,f1,f2, LIF, GIF) where c1, c2 are doubles and
+        c1, c2, f1, f2, LIF, GIF where c1, c2 are doubles and
         f1, f2, LIF, GIF are 2D numpy arrays
 
     References:
@@ -61,19 +61,21 @@ def intensities(I, previousPhi, eps, s, l1, l2):
     oneMH = 1-H
     oneMHI = oneMH*I
     
+    #global intensities
     c1 = np.sum(HI)/np.sum(H)
     c2 = np.sum(oneMHI)/np.sum(oneMH)
     
+    #local intensities
     KHI = cv2.GaussianBlur(HI, ksize = (2*int(s)+1,2*int(s)+1), sigmaX = s, sigmaY =s, borderType=cv2.BORDER_DEFAULT)
     KH = cv2.GaussianBlur(H, ksize = (2*int(s)+1,2*int(s)+1),sigmaX = s,sigmaY = s, borderType=cv2.BORDER_DEFAULT)
     K1HI = cv2.GaussianBlur(oneMHI, ksize = (2*int(s)+1,2*int(s)+1),sigmaX =s, sigmaY =s, borderType=cv2.BORDER_DEFAULT)
     K1H = cv2.GaussianBlur(oneMH, ksize = (2*int(s)+1,2*int(s)+1), sigmaX =s, sigmaY =s, borderType=cv2.BORDER_DEFAULT)
-    
     f1 = KHI/KH
     f2 = K1HI/K1H
-    
+
+    #Local and global intensity forces
+    #convolutions are more efficiently computed in the frequency domain
     GIF = -l1 * (I- c1)*(I-c1) + l2 * (I-c2)*(I-c2)
-    
     LIF = (l2 - l1) * I*I * signal.fftconvolve(Id, K, mode='same')\
     -2 * I * signal.fftconvolve(-l1 * f1 + l2 * f2, K, mode='same')\
     + signal.fftconvolve(-l1 * f1*f1 + l2 * f2*f2, K, mode='same')
@@ -109,8 +111,8 @@ def initiateContour(I, typeC, setPoints = None, param = None):
         the four summets' coordinates, whatever its input value.
         
         param (1D array or list): a = param[0], b = param[1] are the
-        parameters for the line x = ay + b. param[2] defines the width of the
-        quadrangle
+        parameters for the line x = ay + b. param[2] defines the desired
+        width of the quadrangle
 
     Returns:
         array_like of same size than I. Pixels on the contour are zeros,
@@ -175,7 +177,7 @@ def initiateContour(I, typeC, setPoints = None, param = None):
 def activeContour(I, contourIni, thresh, l1, l2, s, eps, mu, nu, dt):
     """Determines the contour of an object in image I by recurrence. This function
     acts like a snake function. The contour evoluates from the input contourIni until 
-    a stationary solution is found, that is:
+    a stationary solution is found, such as:
     |new contour - previous contour| < thresh.
     The chosen norm is the maximum among |new contour (x,y) - previous contour (x,y)| for
     (x,y) in the image domain.
@@ -187,7 +189,7 @@ def activeContour(I, contourIni, thresh, l1, l2, s, eps, mu, nu, dt):
     Args:
         I (array_like): one-canal image
         contourIni (array_like of same size than I): contains the zero level
-        set initial values
+        set initial values. Same format as the output of function initiateContour.
         l1 (double): stricly positive constant
         l2 (double): stricly positive constant
         s (double): standard deviation for gaussian kernels 
@@ -199,7 +201,8 @@ def activeContour(I, contourIni, thresh, l1, l2, s, eps, mu, nu, dt):
         thresh (double): stopping condition for the recurrence
 
     Returns:
-        array_like: stationary contour. The array has the same size than I.
+        array_like: stationary contour represented by pixels = 0.
+        The array has the same size than I.
     
     References:
         Li Wang et al., Computerized Medical Imaging and Graphics 33 (2009) 520–531
@@ -220,11 +223,17 @@ def activeContour(I, contourIni, thresh, l1, l2, s, eps, mu, nu, dt):
     step = 1
     stop_criterion = thresh + 1.
     while stop_criterion > thresh and step <= 10000:
-        if step%10 ==0:
+        if step%10 == 0:
             print('Current tens for step : ', step)
+        
+        #compute global and local intensity forces at step n
         c1, c2, f1, f2, GIF, LIF = intensities(I, previousPhi, eps, s, l1, l2)
 
+        # constant weight for local and global intensity forces
+        # SHOULD BE MODIFIED IN THE FUTURE TO ADAPT EACH IMAGE
         w = 0.01
+
+        # compute mathematical operators at step n
         dirac = eps/(math.pi*(eps*eps+previousPhi*previousPhi))        
         grad = np.gradient(previousPhi)
         normgrad = np.sqrt(grad[0]*grad[0] + grad[1]*grad[1])
@@ -232,50 +241,60 @@ def activeContour(I, contourIni, thresh, l1, l2, s, eps, mu, nu, dt):
         div = np.gradient(grad[0]/normgrad, axis = 0) + np.gradient(grad[1]/normgrad, axis = 1)
         lap = cv2.Laplacian(previousPhi, cv2.CV_64F)
         
+        # compute derivative of contour
         dPhi = dirac * ((1-w)*LIF + w*GIF)\
                 + nu * dirac * div\
                 + mu * (lap - div)
         
+        # compute new contour
         newPhi = previousPhi + dt * dPhi
         
+        # check the stopping criterion
         stop_criterion = np.max(abs(dPhi * dt))
+
+        # go to next step n
         previousPhi = newPhi
-    
         step = step+1
 
     return previousPhi, step-1
 
 def extractContour(levelSet, image, offSetX = 0, offSetY = 0):
     """
-    This function spots the border between neagtive and positive values of
+    This function spots the border between negative and positive values of
     the levelSet object (which represents a zero level set function).
     This border represents the contour of an object in image I.
-    The function returns a image similar to I except that the contour is
-    green-lighted, and a list containing the points of the contour
+    The function returns a list containing the points of the contour and
+    an image similar to I with the contour is green-lighted. 
     
     Args:
         image (array): 3-canal image
         levelSet (array): array of size (image.shape[0], image.shape[1])
-        representing a zero level set function
+        representing a zero level set function (same format as output
+        by activeContour function)
+        offSetX: add an vertical offset (changes rows)
+        offSetY: add an horizontal offSet (changes columns)
     Returns:
-        I (array): array of same size than I, and same pixels value except
+        I (array): array of same size than I, with same pixels value except
         the contour which is green
-        listC (list): list of all pixels from the contour
+        listC (list): list of all pixels contained in the detected contour
     """
     
     listC=[]
     I = np.copy(image) 
       
-    'If several contours detected in levelSet, keep only the biggest'
+    # binarization of the levelset image to separate positive from negative values
     binar = np.uint8((levelSet>=0)*0. + (levelSet<0)*255.)
+    #find contours
     objects = cv2.findContours(binar, mode = cv2.RETR_EXTERNAL, method = cv2.CHAIN_APPROX_NONE)[0]
+    # verify that at least one object exists, otherwise it means that there is no any detected contour in levelset
     if len(objects) == 0 :
         raise ValueError('No contour has been found. Please check: 1) that aponeuroses\
                         have been correctly located. 2) that initial contour is correct')
+    # If several contours detected in levelSet, keep only the biggest
     objects_size = [(i,objects[i].size) for i in range (len(objects))]
     objects_size.sort(key=lambda x:x[1]) 
     biggest = objects_size[-1][0]
-    
+    # verify that the points in the detected contours are within I's dimensions
     for point in objects[biggest]:
         if point[0][0] < I.shape[1] and point[0][1] < I.shape[0]:
             listC.append((point[0][1] + offSetX, point[0][0] + offSetY))
@@ -283,16 +302,14 @@ def extractContour(levelSet, image, offSetX = 0, offSetY = 0):
     return I, listC
 
 def approximateApo(p, apoType, I, typeapprox, d):
-    """Function approximates aponeuroses shape.
+    """Function that approximates an aponeurosis shape.
     More precisely, if apoType is 'upper', meaning an upper aponeurosis
     is being processed, this function will approximate the lower boundary
     of the upper aponeurosis with an interpolated curve of degree d. If apoType 
     is 'lower', meaning a deep aponeurosis is being processed, this function 
     will approximate the upper boundary of the deep aponeurosis with an 
     interpolated curve of degree d.
-    The function returns the spline as well as two one-dimensional
-    arrays containing the x- and y- coordinates of the points
-    approximating the aponeurosis.
+    The function returns the spline that approximates the aponeurosis.
 
     Args:
         p (list): list containing the points of the aponeurosis' contour
@@ -300,11 +317,17 @@ def approximateApo(p, apoType, I, typeapprox, d):
         I (array) : image to which the processed aponeurosis belongs. It
         can either have one canal or three canals.
         d (integer): degree of the interpolated curve.
+        typeapprox (string): 'Bspline' or 'polyfit', to choose which
+            type of interpolation you cant between B splines or polynomial fitting.
+
+    Returns
+        spline modeling aponeurosis.
     """
-    
+
     p.sort(key=lambda x:x[0]) #x-coord sort
     p.sort(key=lambda x:x[1]) #y-coord sort
 
+    # retrieve points from the contour depending on the type of aponeurosis
     if apoType == 'lower':
         #from top to bottom, keep the first contour pixel of each column 
         comparator = -1
@@ -323,15 +346,15 @@ def approximateApo(p, apoType, I, typeapprox, d):
                 line.append(pt)
                 comparator = pt[1]
 
+    # sort aponeurosis' points (necessary for interpolation)
     line.sort(key=lambda x:x[1]) #y-coord sort
     ycoord = [line[i][1] for i in range(len(line))]
     xcoord = [line[i][0] for i in range(len(line))]
 
+    # approximate by a spline, depending on the chosen type of interpolation
     if typeapprox == 'Bspline':
         import scipy.interpolate as interpolate
         spline = interpolate.UnivariateSpline(ycoord, xcoord, k=d, ext = 0) 
-
-
     elif typeapprox == 'polyfit':
         polyn = np.polyfit(ycoord, xcoord, deg = d)
         spline = np.poly1d(polyn)     
