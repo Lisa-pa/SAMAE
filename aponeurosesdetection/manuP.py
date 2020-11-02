055601a4b1acad386dbf6e79a681bcef7526b970
Lisa-pa <65156893+Lisa-pa@users.noreply.github.com>
2020-11-02 13:45:44 +0100

    update docstrings and comments

--- aponeurosesdetection/manuP.py
+++ aponeurosesdetection/manuP.py
@@ -1,12 +1,21 @@
-"""
-Processing of manual data for panoramic images
-Created on Thu Sep  3 16:53:09 2020
-
-@author: Lisa Paillard
-"""
+"""Processing of manual data for panoramic images"""
 
 def panoManu(manu_pts_dict, path_img = None):
     """
+    Function that computes the architecture of a panoramic image from
+    manually labelled data.
+    
+    Inputs:
+        manu_pts_dict (dict): dictionary that contains the manual data
+            and on which the manual architecture will be written.
+            For further details on the structure of this dictionary, 
+            see function idfascicles in arch.py
+        path_img (optional string): this path leads to the image and
+            allows to visualize aponeuroses and fascicles on the image
+
+    Outputs:
+        manu_pts_dict (dict) modified with the computation of architecture
+            from manual data.
     """
     import aponeurosesdetection.MUFeaM as MUFeaM
     import numpy as np
@@ -17,7 +26,7 @@ def panoManu(manu_pts_dict, path_img = None):
     calib = manu_pts_dict['calfct_to_mm']
     pt_inter = manu_pts_dict['insertion']['coords']
 
-    #aponeuroses interpolation with Bspline
+    #interpolation of aponeuroses manual points with Bspline
     list_sup = list(manu_pts_dict['aposup']['coords'])
     list_sup.sort(key=lambda x:x[1])
     y_sup = [list_sup[i][1] for i in range(len(list_sup))]
@@ -33,8 +42,7 @@ def panoManu(manu_pts_dict, path_img = None):
     coordSup, spline_sup = MUFeaM.pointsCoordinates(typeA = 'spline', param = spline_sup, interval = [0, int(pt_inter[1])])
     coordInf, spline_inf = MUFeaM.pointsCoordinates(typeA = 'spline', param = spline_inf, interval = [0, int(pt_inter[1])])
 
-
-    # features computation
+    # interpolation of fascicles manual points, and features computation
     abscissa, thickness, splineT = MUFeaM.muscleThickness(start = max(min(y_sup), min(y_inf)), end = min(max(y_sup), max(y_inf)), calibV = calib, calibH = calib, spl1 = spline_sup, spl2 = spline_inf)
     manu_pts_dict['MT'] = {'coords': thickness, 'columns interval': [max(min(y_sup), min(y_inf)), min(max(y_sup), max(y_inf))]}
     
@@ -44,8 +52,13 @@ def panoManu(manu_pts_dict, path_img = None):
         #Bspline
         spl = FaDe.approximateFasc(typeapprox = 'Bspline', listF = [fasci], d = 1)
         #find intersection points with aponeuroses
+        #determination of the dominant orientation of fascicles (positive or negative slope)
+        if (fasci[1][1]-fasci[0][1]) / (fasci[1][0]-fasci[0][0])<0:
+            sig = -1
+        elif (fasci[1][1]-fasci[0][1]) / (fasci[1][0]-fasci[0][0])>0:
+            sig = 1
         inters_inf, inters_sup, spl = MUFeaM.findIntersections(spl_inf = spline_inf, spl_sup = spline_sup,\
-                                                               listSpl = spl, search_interval = [0, pt_inter[1]], start=0)
+                                                               listSpl = spl, signOfSlope = sig, search_interval = [0, pt_inter[1]], start=0)
         if len(spl)>0:
             #find pennation angles
             PA_sup = MUFeaM.pennationAngles(spl_a = spline_sup, listS_f = spl, listI = inters_sup, xcalib = calib, ycalib =  calib)
@@ -54,6 +67,7 @@ def panoManu(manu_pts_dict, path_img = None):
             FL = MUFeaM.fasciclesLength(listS_f = spl, listIu = inters_sup, listId = inters_inf, xcalib = calib, ycalib = calib)
             loc = MUFeaM.locateFasc(inters_inf, refPoint = pt_inter, ycalib = calib)
             
+            # update dictionary
             manu_pts_dict[fsc]['dist from insertion in mm'] = loc[0]
             manu_pts_dict[fsc]['PAsup'] = {'value in degree' : PA_sup[0],
                          'intersection with apo': inters_sup[0]}
@@ -61,6 +75,7 @@ def panoManu(manu_pts_dict, path_img = None):
                          'intersection with apo': inters_inf[0]}
             manu_pts_dict[fsc]['FL'] = {'length in mm': FL[0]}
 
+        #optional visualization
         if path_img is not None: 
             import cv2
             #fascicles visualization
--- aponeurosesdetection/manuS.py
+++ aponeurosesdetection/manuS.py
@@ -1,19 +1,28 @@
-"""
-Processing of manual data for simple/standard images
-Created on Thu Sep  3 16:52:58 2020
+"""Processing of manual data for simple/standard images"""
 
-@author: Lisa Paillard
-"""
-
-def simpleManu(manu_pts_dict, path_img = None):
+def simpleManu(manu_pts_dict):
+    """
+    Function that computes the architecture of a simple image from
+    manually labelled data.
     
+    Inputs:
+        manu_pts_dict (dict): dictionary that contains the manual data
+            and on which the manual architecture will be written.
+            For further details on the structure of this dictionary, 
+            see function idfascicles in arch.py
+
+    Outputs:
+        manu_pts_dict (dict) modified with the computation of architecture
+            from manual data.
+    """
     import aponeurosesdetection.MUFeaM as MUFeaM
     import numpy as np
     import aponeurosesdetection.FaDe as FaDe
     import fnmatch
 
     calib = manu_pts_dict['calfct_to_mm']
-    
+
+    #interpolation of aponeuroses manual points with 1-degree polynomial fitting    
     list_sup = list(manu_pts_dict['aposup']['coords'])
     list_sup.sort(key=lambda x:x[1])
     y_sup = [list_sup[i][1] for i in range(len(list_sup))]
@@ -23,25 +32,29 @@ def simpleManu(manu_pts_dict, path_img = None):
     list_inf.sort(key=lambda x:x[1])
     y_inf = [list_inf[i][1] for i in range(len(list_inf))]
     x_inf = [list_inf[i][0] for i in range(len(list_inf))]
-
-    #interpolation with Bspline        
+   
     polyn_sup = np.polyfit(y_sup, x_sup, deg = 1)
     spline_sup = np.poly1d(polyn_sup)
     polyn_inf = np.polyfit(y_inf, x_inf, deg = 1)
     spline_inf = np.poly1d(polyn_inf)
     
-    # features computation
+    # interpolation of fascicles manual points, and features computation
     abscissa, thickness, splineT = MUFeaM.muscleThickness(start = max(min(y_sup), min(y_inf)), end = min(max(y_sup), max(y_inf)), calibV = calib, calibH = calib, spl1 = spline_sup, spl2 = spline_inf)
     manu_pts_dict['MT'] = {'coords': thickness, 'columns interval': [max(min(y_sup), min(y_inf)), min(max(y_sup), max(y_inf))]}
     
     for fsc in fnmatch.filter(manu_pts_dict.keys(), 'fsc_*'):
         fasci = manu_pts_dict[fsc]['coords']        
 
-        #Bspline
+        # polynomial fitting
         spl = FaDe.approximateFasc(typeapprox = 'polyfit', listF = [fasci], d = 1)
         #find intersection points with aponeuroses
+        #determination of the dominant orientation of fascicles (positive or negative slope)
+        if (fasci[1][1]-fasci[0][1]) / (fasci[1][0]-fasci[0][0])<0:
+            sig = -1
+        elif (fasci[1][1]-fasci[0][1]) / (fasci[1][0]-fasci[0][0])>0:
+            sig = 1
         inters_inf, inters_sup, spl = MUFeaM.findIntersections(spl_inf = spline_inf, spl_sup = spline_sup,\
-                                                               listSpl = spl, search_interval = [-200/calib,200/calib], start = 0)
+                                                               listSpl = spl, signOfSlope = sig, search_interval = [-200/calib,200/calib], start = 0)
         if len(spl)>0:
             #find pennation angles
             PA_sup = MUFeaM.pennationAngles(spl_a = spline_sup, listS_f = spl, listI = inters_sup, xcalib = calib, ycalib = calib)
@@ -50,6 +63,7 @@ def simpleManu(manu_pts_dict, path_img = None):
             FL = MUFeaM.fasciclesLength(listS_f = spl, listIu = inters_sup, listId = inters_inf, xcalib = calib, ycalib = calib)
             loc = MUFeaM.locateFasc(inters_inf, [0,0], calib)
             
+            #update dictionary
             manu_pts_dict[fsc]['dist from (0,0) of RGB image, in mm'] = loc[0]
             manu_pts_dict[fsc]['PAsup'] = {'value in degree' : PA_sup[0],
                          'intersection with apo': inters_sup[0]}
