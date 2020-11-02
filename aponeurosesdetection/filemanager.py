2d9fc783e79102665035459f2e678b46891e90b2
Lisa-pa <65156893+Lisa-pa@users.noreply.github.com>
2020-11-02 11:27:39 +0100

    update comments

--- aponeurosesdetection/filemanager.py
+++ aponeurosesdetection/filemanager.py
@@ -1,11 +1,14 @@
+#import packages and functions
 from pathlib import Path
-from aponeurosesdetection.plot import plotFeatures
-from aponeurosesdetection.arch import dame_arch_paths, dame_arch_data
+from plot import plotFeatures
+from arch import dame_arch_paths, dame_arch_data
+#####################################################################################
+# MANUAL INPUTS REQUIRED FROM USER
 
-# manually change the path
+# change the path
 path_to_folders = str(Path(r'C:\Users\Lisa Paillard\Desktop\Pour TFE\jHamON_data'))
 
-# participants - adapt the list manually
+# participants - adapt the list
 part = ['01_Kevin', '02_rafaelopes', '03_charlesbarrand', '04_guilhem',\
         '05_leandre', '06_thomasmartine', '10_victor',\
         '11_youssouf', '12_sufyan', '16_julien', '34_nicolas']
@@ -16,11 +19,11 @@ colors = [(1,0.26,0.11),(0.45,0.45,1),(0.8,0.2,1),(0,0.4,1),\
           (1,0,0.4),(0.7,0.24,0),(0,0,0.8)]
 
 #####################################################################################
-#launch analysis
+# launch analysis automatically
 archpaths = dame_arch_paths(path_to_folders=path_to_folders, participants = part)
 arch_dict = dame_arch_data(archpaths)
 
-#save results
+# save results in dict object
 from dictmanager import save_obj
 save_obj(arch_dict, path_to_folders + '\\results')
 
